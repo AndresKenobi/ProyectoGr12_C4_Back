19042343a5c9c8c3d6f8b0e465080c14ec5c1cfe
Leo <ing.victorerazo@hotmail.com>
2021-11-27 17:20:04 -0500

    ajuste schema usuarios y confirm  consola registro

--- models/usuarios.js
+++ models/usuarios.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const usuariosSchema = mongoose.Schema({
       name: String,
-      address: String,
-      city: String,
-      state: String,
-      zip: String,
-      country: String,
+      //address: String,
+      //city: String,
+      //state: String,
+      //zip: String,
+      //country: String,
       // Agregar correo y clave a la interfaz, por que son los datos de validacion
       email: String,
       password: String
